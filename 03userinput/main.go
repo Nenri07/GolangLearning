74d5f0c910ae1055415b67a6ccf2f3f58f9a162d
Nenri07 <umarbashir2002@gmail.com>
2026-04-21 06:24:24 +0500

    Implement user input functionality and display skill rating in main.go

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
-func main(){
-	
+func main() {
+	welcome := "Welcome to Input class"
+	fmt.Println(welcome)
+
+	read := bufio.NewReader(os.Stdin)
+	fmt.Println("enter my skill on golang")
+	//comma ok syntax|| error syntax
+	input, _ := read.ReadString('\n')
+
+	fmt.Println("this is rating of my skill", input)
+	fmt.Printf("this is type of input %T \n", input)
+
 }
