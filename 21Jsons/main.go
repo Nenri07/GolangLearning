bef9c3928c7bdd267e379286366b9b49577bdf5f
Nenri07 <umarbashir2002@gmail.com>
2026-05-30 09:53:39 +0500

    interface, andjsons

---
+++ 21Jsons/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("this is json class")
+	coursesJson()
+}
+
+type course struct {
+	Name  string   `json:"coursename"`
+	Price int      `json:"coursePrice"`
+	Tags  []string `json:"tagsofcourse,omitempty"`
+	Pass  string   `json:"-"`
+	Tutor string   `json:"tutorName"`
+}
+
+func coursesJson() {
+	courses := []course{
+		{"golang", 24, []string{"gin", "go", "multi"}, "22Arid4340", "nenrei"},
+		{"nextjs", 24, []string{"tsx", "js", "web server side rendering"}, "Arid4340", "nenrei007"},
+		{"Japnease", 24, []string{"jlpt", "nihongo"}, "4340", "nenreisensei"},
+		{"postgre", 22, nil, "22Arid4340", "nenrei"},
+	}
+	// interfaces are actually just a struct simply
+	jsonData, err := json.MarshalIndent(courses, "", "\t")
+
+	CheckNillError(err)
+
+	fmt.Println("this is data from the jsonconverter marshal", string(jsonData))
+	fmt.Printf("%s\n This is data f pf", jsonData)
+}
+func CheckNillError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
