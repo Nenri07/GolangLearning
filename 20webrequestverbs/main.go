b85d1d12d30b538969b9a96809a9a0c1ef31f1f0
Nenri07 <umarbashir2002@gmail.com>
2026-05-30 07:08:52 +0500

    more requests

--- 20webrequestverbs/main.go
+++ 20webrequestverbs/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("This is another request class with our own server in node")
 	GetData()
+	postdata()
+	postFormData()
 }
 
 func GetData() {
@@ -35,4 +38,46 @@ func CheckNillError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func postdata() {
+	const url string = "http://localhost:8000/post"
+
+	body := strings.NewReader(`
+	{
+	"name":"nenrei",
+	"lang":"Japnease"
+	}
+	`)
+
+	response, err := http.Post(url, "application/json", body)
+	CheckNillError(err)
+	fmt.Println("this is raw response", response)
+
+	defer response.Body.Close()
+
+	bytedata, _ := io.ReadAll(response.Body)
+
+	fmt.Println("this is all formated data", string(bytedata))
+
+}
+
+func postFormData() {
+	const murl string = "http://localhost:8000/postform"
+	//formdata preparing
+	formdata := url.Values{}
+
+	formdata.Add("name", "nenrei")
+	formdata.Add("email", "streamsy.dev")
+	formdata.Add("age", "22")
+
+	res, err := http.PostForm(murl, formdata)
+
+	CheckNillError(err)
+	defer res.Body.Close()
+
+	byteData, _ := io.ReadAll(res.Body)
+
+	fmt.Println("this is formdata with response", string(byteData))
+
 }
